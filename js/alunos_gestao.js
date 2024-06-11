eac4b1fa4c397967cfe32ee85a67bcab729db021
Carolina Neponucena <cneponucena@gmail.com>
2024-06-11 09:56:26 -0300

    modal de aluno

--- js/alunos_gestao.js
+++ js/alunos_gestao.js
@@ -70,3 +70,20 @@ function formatarData(dataString) {
 
     return `${dia}/${mes}/${ano}`;
 }
+var btn = document.querySelector(".new-class-button");
+var modal = document.getElementById("myModal");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
